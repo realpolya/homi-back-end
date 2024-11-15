cb5d12cad0424e9cbc1bb04f4d94e8dd3d7ba79b
realpolya <ps2780@columbia.edu>
2024-11-15 17:44:42 -0500

    change the location of profile model

--- bookings/models.py
+++ bookings/models.py
@@ -1,14 +1,1 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    googleId = models.CharField(max_length=100, default="")
-    profile_pic = models.CharField(max_length=250, default="")
-    bio = models.TextField(default="")
-    is_host = models.BooleanField(default=False)
-    profits = models.FloatField(default=0)
-
-    def __str__(self):
-        return f"{self.user.username}'s profile"
