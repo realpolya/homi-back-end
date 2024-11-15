5ad3631c419bda157b0d400dd0298dd29c165421
MDeadwiler <mdeadwiler@email.com>
2024-11-15 17:47:38 -0500

    Profile-model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    googleId = models.CharField(default="")
+    profile_pic = models.CharField(default="")
+    bio = models.CharField(default="")
+    is_host = models.BooleanField(default=False)
+    profits = models.FloatField(default=0)
+    
+    def __str__(self):
+        return f"{self.user.username}'s profile"
