582a707253e8384f67d14af86bb80fe8c2f76010
realpolya <ps2780@columbia.edu>
2024-11-15 18:26:58 -0500

    update tuple, work on address

--- properties/models.py
+++ properties/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+
 from .amenities import AMENITIES
+from .states import STATES
 from .property_type import PROPERTY_TYPE
 from .cancellation import CANCELLATION
-from django.contrib.postgres.fields import ArrayField
-
-
-# Create your models here.
 
 
 class Property(models.Model):
     title = models.CharField(max_length=50)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField(max_length=500)
     price_per_night = models.IntegerField()
     max_guests = models.IntegerField()
@@ -18,8 +19,6 @@ class Property(models.Model):
         choices=PROPERTY_TYPE,
         default=PROPERTY_TYPE[0] 
     )
-    
-
     amenities = ArrayField(
             models.CharField(max_length=50, choices=AMENITIES), 
             blank=True
@@ -30,7 +29,6 @@ class Property(models.Model):
         max_length=20,
         choices=CANCELLATION
     )
-
     photos = ArrayField(
         models.URLField(max_length=200),
         blank=True
@@ -39,6 +37,19 @@ class Property(models.Model):
     def __str__(self):
         return self.title
 
-    
+
+class Address(models.Model):
+    prop = models.OneToOneField(Property, on_delete=models.CASCADE)
+    street = models.CharField(max_length=250)
+    city = models.CharField(max_length=250)
+    state = models.CharField(
+        max_length=250,
+        choices=STATES,
+        default=STATES[0] 
+    )
+    zip_code = models.CharField(max_length=6)
+    country = models.CharField(max_length=20, default="United States")
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
 
 
