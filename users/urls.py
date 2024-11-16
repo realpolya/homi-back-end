fda54b1ed475c729a82f1ba7ad14846b73e494e0
Rani Bungay <ranibungay@gmail.com>
2024-11-16 17:31:18 -0500

    create route to update profile

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from .views import CreateUserView, LoginView, VerifyUserView
+from .views import CreateUserView, LoginView, VerifyUserView, UserProfileView
 from django.urls import path
 
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path('users/register/', CreateUserView.as_view(), name='register' ), 
     path('users/login/', LoginView.as_view(), name='login'),
     path('users/token/refresh/', VerifyUserView.as_view(), name='token_refresh'),
+    path('users/profile/', UserProfileView.as_view(), name='user-profile'),
 ]
