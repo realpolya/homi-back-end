16b07eb17a8468745a622b2ad4984c91b6d904c8
realpolya <ps2780@columbia.edu>
2024-11-17 12:30:59 -0500

    update property serializer

--- properties/serializers.py
+++ properties/serializers.py
@@ -2,17 +2,18 @@ from rest_framework import serializers
 from .models import Property, Photo, Address
 from amenities.serializers import AmenitySerializer
 
-class PropertySerializer(serializers.ModelSerializer):
-    amenities = AmenitySerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Property
-        fields = '__all__'
-
 class AddressSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Address
+        fields = '__all__'
+
+class PropertySerializer(serializers.ModelSerializer):
+    amenities = AmenitySerializer(many=True, read_only=True)
+    address = AddressSerializer(read_only=True)
+
+    class Meta:
+        model = Property
         fields = '__all__'
 
 class PhotoSerializer(serializers.ModelSerializer):
