7ee0c7923d745b9764ba3e8154b866355304afda
realpolya <ps2780@columbia.edu>
2024-11-17 15:55:32 -0500

    create properties user and properties one

--- properties/urls.py
+++ properties/urls.py
@@ -4,5 +4,7 @@ from .views import *
 # allows to view all amenities, query amenities, create a new amenity
 urlpatterns = [
     path('', PropertiesList.as_view(), name='properties-list'),
-    path('<int:id>/', PropertiesOne.as_view(), name='properties-one')
+    path('mine/', PropertiesMine.as_view(), name='properties-mine'),
+    path('user/<int:user_id>/', PropertiesUser.as_view(), name='properties-user'),
+    path('<int:id>/', PropertiesOne.as_view(), name='properties-one'),
 ]
