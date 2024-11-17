5b60286ef6a3b7628efe734f646ec453ee8b19d7
realpolya <ps2780@columbia.edu>
2024-11-17 12:28:44 -0500

    create a migration, work on serializer for property

---
+++ properties/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Property, Photo, Address
+from amenities.serializers import AmenitySerializer
+
+class PropertySerializer(serializers.ModelSerializer):
+    amenities = AmenitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Property
+        fields = '__all__'
+
+class AddressSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Address
+        fields = '__all__'
+
+class PhotoSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Photo
+        fields = '__all__'
